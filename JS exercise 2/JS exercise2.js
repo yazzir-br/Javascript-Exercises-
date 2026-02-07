0b799e6efbb5a8a3842d486c7b1c76b7784bc879
Your Name <yazziribra@gmail.com>
2026-02-07 08:27:28 -0800

    JS exercise 2

---
+++ JS exercise 2/JS exercise2.js
@@ -0,0 +1,19 @@
+let a = 17;
+let b = "17";
+
+console.log( a == b);
+
+console.log( a != b);
+
+console.log(a === b);
+
+console.log(a !== b);
+
+console.log(a > b);
+
+console.log(a < b);
+
+console.log(a >= b);
+
+console.log(a <= b);
+
