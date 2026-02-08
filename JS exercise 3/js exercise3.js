b8603365998acffa2e9658cdc3617f744957c4dc
Your Name <yazziribra@gmail.com>
2026-02-08 06:23:39 -0800

    JS esexrce #

---
+++ JS exercise 3/js exercise3.js
@@ -0,0 +1,15 @@
+let isSunny = true;
+let isWeekend = false;
+
+// AND &&
+
+console.log(isSunny && isWeekend);
+
+// OR
+
+console.log( isSunny || isWeekend);
+
+// NOT
+
+console.log(! isSunny)
+console.log(! isWeekend)
