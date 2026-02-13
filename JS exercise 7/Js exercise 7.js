adb1e7515531b49da40b76e9497fd4fa9addfefc
Your Name <yazziribra@gmail.com>
2026-02-13 05:46:46 -0800

    JS exercise 7

---
+++ JS exercise 7/Js exercise 7.js
@@ -0,0 +1,11 @@
+let colours  = ["green", "white", "blue"];
+
+//  Adding clour on last 
+colours.push("yellow");
+console.log(colours);
+
+//  Removing first colour
+colours.shift();
+console.log(colours);
+
+console.log(colours.length); 
