15d32590c57ab909ca4dd85a3da66ecbcb3ec0d9
Your Name <yazziribra@gmail.com>
2026-02-13 11:06:46 -0800

    JS exercise 10

---
+++ JS exercise 10/Js exercise 10.js
@@ -0,0 +1,18 @@
+
+let library = [
+    { title: "BBC",
+      author: "Ali",
+      year: 2010 
+    },
+
+    { title: "Baro",
+      author: "Ilmi",
+      year: 2013
+    },
+
+];
+
+console.log(library[0].title);
+
+console.log(library[1].author); 
+
