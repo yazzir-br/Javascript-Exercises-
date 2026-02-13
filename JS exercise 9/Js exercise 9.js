161f9ed9acd255b2ac2134bb30c078271926b16d
Your Name <yazziribra@gmail.com>
2026-02-13 09:39:07 -0800

    JS exercise 9

---
+++ JS exercise 9/Js exercise 9.js
@@ -0,0 +1,19 @@
+
+
+let car = {
+    make: "Jpan",
+    model: 109,
+    year: "2029",
+
+    start : function() {
+        console.log( "the car has started")   
+    }
+};
+
+console.log(car.make); 
+console.log(car.model);
+console.log(car.year); 
+
+console.log( car.start());
+
+
