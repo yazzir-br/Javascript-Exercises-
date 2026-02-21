65c9d0334095e1acbdf52e70200586f51bd057fd
Your Name <yazziribra@gmail.com>
2026-02-21 00:31:44 -0800

    Js exercise 17

---
+++ JS exercise 17/Js exercise 17.js
@@ -0,0 +1,12 @@
+
+let temperature  = 13
+
+if ( temperature < 0){
+    console.log("its very cold")
+}else if ( temperature < 15) {
+    console.log("its cold")
+}else if ( temperature < 25) {
+    console.log("its Warm")
+}else{
+    console.log("its Hot")
+}
