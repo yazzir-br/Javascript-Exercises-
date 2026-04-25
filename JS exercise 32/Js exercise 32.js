7a64ac3869ac46ff82430125ace5f4c2c1f5a110
Your Name <yazziribra@gmail.com>
2026-04-25 05:17:23 -0700

    Js exrcise 32

---
+++ JS exercise 32/Js exercise 32.js
@@ -0,0 +1,10 @@
+
+
+const ciwaan =document.querySelector('h1')
+console.log( ciwaan)
+
+const magac = document.querySelector('#muuse')
+console.log( magac)
+
+const text = document.querySelectorAll('.qoraal')
+console.log(text)
