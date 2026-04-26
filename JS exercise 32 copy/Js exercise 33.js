7af6943ef110456355384048a37a299aa5229378
Your Name <yazziribra@gmail.com>
2026-04-26 00:40:09 -0700

    JS exercise 33

---
+++ JS exercise 32 copy/Js exercise 33.js
@@ -0,0 +1,14 @@
+
+
+
+function changeText() {
+  
+    const text1 = document.querySelector('.text1');
+    text1.textContent = "HADDA WAXA IS BADALAY TEXT";
+}
+
+function changeElement() {
+
+    const text2 = document.querySelector('.text2');
+    text2.innerHTML = `Hadda waxa is badalay <strong>ELEMENT GII</strong>`;
+}
